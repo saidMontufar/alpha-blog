1b02c9ed14b8d424b4b7ec492ce7c99e147d5078
said.montufar <said.montufar@outlook.com>
2022-11-18 21:59:45 -0500

    Add layout links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,39 +1,53 @@
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.find(params[:id])
-    end
+  #--> Sets article before the actions mentioned in the square brackets
+  #--> This is defined on line 46 under private
+  before_action :set_article, only: %i[show edit update destroy]
 
-    def index
-        @articles = Article.all
-    end
+  def show
+  end
 
-    def new
-        @article = Article.new
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def edit
-        @article = Article.find(params[:id])
-    end
+  def new
+    @article = Article.new
+  end
 
-    def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
-            flash[:notice] = "Article was created succesfully"
-            redirect_to @article
-        else
-            render :edit, status: :unprocessable_entity
-        end
-    end
+  def edit
+  end
 
-    def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
-        if @article.save
-            flash[:notice] = "Article was created succesfully."
-            redirect_to @article
-        else
-            render :new, status: :unprocessable_entity
-        end
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was created succesfully"
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created succesfully."
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private #--> These functions are exclusve to this controller file
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'index', to: 'articles#index'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update] #--> Imports only the :show action
+  root "pages#home"
+  get "about", to: "pages#about"
+  get "index", to: "articles#index"
+  get "delete", to: "articles#delete"
+  resources :articles
 end
-
-
